282f2a500bc59927ce34db5122e0830390e34063
teofalk99 <85136351+teofalk99@users.noreply.github.com>
2022-12-01 16:47:36 -0500

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -17,12 +17,50 @@ const rating_min = document.getElementById('rating_min')
 const skill = document.getElementById('skill')
 const course = document.getElementById('course')
 const sort = document.getElementById('sort')
+const error_field = document.getElementById('error')
 
 
 function check_data()
 {
     //check that inputs are proper
-    return
+    errors = []
+    if (rating_min.value && parseInt(rating_min.value) > 100)
+    {
+        errors.push("Minimum rating too high")
+    }
+    if ((cal_min.value && cal_max.value) && parseInt(cal_min.value) > parseInt(cal_max.value))
+    {
+        errors.push("Minimum calories cannot be higher than maximum")
+    }
+    if ((protein_min.value && protein_max.value) && parseInt(protein_min.value) > parseInt(protein_max.value))
+    {
+        errors.push("Minimum protein cannot be higher than maximum")
+    }
+    if ((carb_min.value && carb_max.value) && parseInt(carb_min.value) > parseInt(carb_max.value))
+    {
+        errors.push("Minimum carbohydrates cannot be higher than maximum")
+    }
+    if ((fat_min.value && fat_max.value) && parseInt(fat_min.value) > parseInt(fat_max.value))
+    {
+        errors.push("Minimum fat cannot be higher than maximum")
+    }
+
+    if (errors.length > 0)
+    {
+        error_message = "Please fix following errors: "
+
+        errors.forEach(m => {
+            error_message += (m + " - ")
+        });
+
+
+        error_field.innerHTML = error_message
+    }
+    else
+    {
+        error_field.innerHTML = ""
+        send_data();
+    }
 }
 
 
